a36a9e43d0ebe9b2b1853eeb8e56428eda6685f3
DonnaThompson7 <130340823+DonnaThompson7@users.noreply.github.com>
2023-09-09 22:15:07 -0400

    moved proj title to static visible position on card to meet Requirements

--- src/components/pages/ProjectDetails.js
+++ src/components/pages/ProjectDetails.js
@@ -3,13 +3,13 @@ import { Col } from "react-bootstrap";
 export const ProjectDetails = ({ title, description, imgUrl, deployedUrl, repoUrl }) => {
   return (
     <Col xl={4} md={6} sm={12}>
+      <h4 className="proj-title">{title}</h4>
       <div className="proj-imgbx">
         <img src={imgUrl} />
         <div className="proj-txtx">
-          <h4>{title}</h4>
           <span>{description}</span>
-          <div><a href={deployedUrl}>Link to Deployed App</a></div>
-          <a href={repoUrl}>Link to GitHub Repository</a>
+          <div className="proj-link"><a href={deployedUrl}>Link to Deployed App</a></div>
+          <div className="proj-link"><a href={repoUrl}>Link to GitHub Repository</a></div>
         </div>
       </div>
     </Col>
